1b9f2b7e71037b219ae8561e4e764bc8c3eab72e
Pratyanj <109451378+pratyanj@users.noreply.github.com>
2024-12-26 12:26:35 +0530

    paldex page component

---
+++ components/micro/filter/button.tsx
@@ -0,0 +1,35 @@
+import { Image } from "expo-image";
+import React from "react";
+import { TouchableOpacity, StyleSheet } from "react-native";
+
+interface FilterButtonProps {
+  img: string;
+  isSelected?: boolean;
+  onPress: () => void;
+}
+
+export function FilterButton({ img, isSelected, onPress }: FilterButtonProps) {
+  console.log(isSelected);
+  return (
+    <TouchableOpacity
+      style={[
+        styles.container,
+        { backgroundColor: isSelected ? "#3b778d" : "#1a2735" },
+      ]}
+      onPress={onPress}
+    >
+      <Image source={img} style={{ width: 24, height: 24 }} />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 44,
+    height: 44,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 4,
+  },
+});
--- components/palcrad.tsx
+++ components/palcrad.tsx
@@ -2,16 +2,66 @@ import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { Image } from "expo-image";
 import { Colors } from "../constants/Colors";
+import { ThemeContext } from "../constants/ThemeContext";
 type Props = {
   name: string;
   image: string;
   number: string;
   element : string;
 };
-const Theme = {mode: "dark"};
-let actColor = Colors[Theme.mode];
 export default function Card11({ name, image, number,element }: Props) {
-  
+  const { theme } = React.useContext(ThemeContext);
+  const actColor = Colors[theme.mode];
+
+  const styles = StyleSheet.create({
+    container: {
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: 10,
+      
+    },
+    imageContainer: {
+      width: 165,
+      height: 165,
+      borderRadius: 8,
+      backgroundColor: actColor.outlineVariant,
+    },
+    infoContainer: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      gap: 10,
+      alignItems: "center",
+      position: 'absolute', 
+      bottom: 0, 
+      left: 0,
+      right: 0,
+      width:'100%',
+      backgroundColor: 'rgba(0,0,0,0.5)',
+      borderRadius: 8,
+      padding: 3,
+    },
+    image: {
+      width: "100%",
+      height: "100%",
+    },
+    name: {
+      fontSize: 14,
+      color:"white",
+      fontWeight: "bold",
+    },
+    number: {
+      fontSize: 12,
+    },
+    iconimg:{
+      position: 'absolute',
+      left: 3,
+      bottom: 138,
+      width: 24,
+      height: 24,
+      borderTopLeftRadius: 8,
+    },
+  });
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
@@ -28,52 +78,4 @@ export default function Card11({ name, image, number,element }: Props) {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 10,
-    
-  },
-  imageContainer: {
-    width: 165,
-    height: 165,
-    borderRadius: 8,
-    backgroundColor: actColor.outlineVariant,
-  },
-  infoContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    gap: 10,
-    alignItems: "center",
-    position: 'absolute', 
-    bottom: 0, 
-    left: 0,
-    right: 0,
-    width:'100%',
-    backgroundColor: 'rgba(0,0,0,0.5)',
-    borderRadius: 8,
-    padding: 3,
-  },
-  image: {
-    width: "100%",
-    height: "100%",
-  },
-  name: {
-    fontSize: 14,
-    color:"white",
-    fontWeight: "bold",
-  },
-  number: {
-    fontSize: 12,
-  },
-  iconimg:{
-    position: 'absolute',
-    left: 3,
-    bottom: 138,
-    width: 24,
-    height: 24,
-    borderTopLeftRadius: 8,
-  },
-});
+
