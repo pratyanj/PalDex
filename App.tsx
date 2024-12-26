2834493def00c0509ae92677b0fcf485db4d9ae2
Pratyanj <109451378+pratyanj@users.noreply.github.com>
2024-12-26 12:24:28 +0530

    add paper provider to app

--- App.tsx
+++ App.tsx
@@ -1,24 +1,30 @@
-import PalList from "../PalDex/app/index";
+import PalList from "./app/(tabs)";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { ThemeContext } from "./constants/ThemeContext";
 import { useState } from "react";
-
+import { PaperProvider } from "react-native-paper";
+import { Appearance } from "react-native";
 export default function App() {
-  const [theme, setTheme] = useState({ mode: "dark" });
-  
+  const [theme, setTheme] = useState<{ mode: "light" | "dark" }>({
+    mode: "dark",
+  });
   const updateTheme = (newTheme?: { mode: "light" | "dark" }) => {
     if (!newTheme) {
       const nextMode = theme.mode === "light" ? "dark" : "light";
       setTheme({ mode: nextMode });
+
+      Appearance.setColorScheme(nextMode);
     } else {
       setTheme(newTheme);
+      Appearance.setColorScheme(newTheme.mode);
     }
   };
-
   return (
     <ThemeContext.Provider value={{ theme, updateTheme }}>
       <GestureHandlerRootView style={{ flex: 1 }}>
-        <PalList />
+        <PaperProvider>
+          <PalList />
+        </PaperProvider>
       </GestureHandlerRootView>
     </ThemeContext.Provider>
   );
