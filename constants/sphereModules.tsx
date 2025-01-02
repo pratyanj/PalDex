d6dc2ddeee7a52408829a8afddbfe1454047c270
Pratyanj <109451378+pratyanj@users.noreply.github.com>
2025-01-02 12:17:44 +0530

    add data list of weapon,sphere module,allItem

---
+++ constants/sphereModules.tsx
@@ -0,0 +1,261 @@
+export const sphereModule = [
+  {
+    ID: 1,
+    name: "Heavy Weight Module",
+    rarity: "Uncommon",
+    technology: 11,
+    description:
+      "Equipping it makes the sphere heavier, reducing its range but increasing its capture power.Sphere Weight +1 Capture Strength +1",
+    effects: ["Sphere Weight +1", "Capture Strength +1"],
+    image: require("../assets/images/items/heavy-weight-module.png"),
+    image_url:
+      "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_SphereModule_Heavy.webp",
+    recipe: [
+      {
+        name: "Paldium Fragment",
+        quantity: 10,
+        image: require("../assets/images/items/paldium-fragment.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_Pal_crystal_S.webp",
+      },
+      {
+        name: "Ingot",
+        quantity: 10,
+        image: require("../assets/images/items/ingot.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_CopperIngot.webp",
+      },
+      {
+        name: "Stone",
+        quantity: 20,
+        image: require("../assets/images/items/stone.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_Stone.webp",
+      },
+    ],
+  },
+  {
+    ID: 2,
+    name: "Curve Module",
+    rarity: "Rare",
+    technology: 23,
+    description:
+      "Equipping it causes the sphere to spin with a curved trajectory. \r\nIt becomes easier to catch Pals off guard, and increases capture power.Sphere Curve +1 Capture Strength +2",
+    effects: ["Sphere Curve +1", "Capture Strength +2"],
+    image: require("../assets/images/items/curve-module.png"),
+    image_url:
+      "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_SphereModule_Curve.webp",
+    recipe: [
+      {
+        name: "Paldium Fragment",
+        quantity: 30,
+        image: require("../assets/images/items/paldium-fragment.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_Pal_crystal_S.webp",
+      },
+      {
+        name: "Ingot",
+        quantity: 30,
+        image: require("../assets/images/items/ingot.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_CopperIngot.webp",
+      },
+      {
+        name: "Stone",
+        quantity: 50,
+        image: require("../assets/images/items/stone.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_Stone.webp",
+      },
+      {
+        name: "Ancient Civilization Parts",
+        quantity: 5,
+        image: require("../assets/images/items/ancient-civilization-parts.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_PalCrystal_Ex.webp",
+      },
+    ],
+  },
+  {
+    ID: 3,
+    name: "Sniper Module",
+    rarity: "Epic",
+    technology: 31,
+    description:
+      "Equipping it increases the sphere's range, allowing it to sharply capture Pals and enhancing capture power.Sphere Range +1 Capture Strength +2",
+    effects: ["Sphere Range +1", "Capture Strength +2"],
+    image: require("../assets/images/items/sniper-module.png"),
+    image_url:
+      "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_SphereModule_Spiper.webp",
+    recipe: [
+      {
+        name: "Paldium Fragment",
+        quantity: 50,
+        image: require("../assets/images/items/paldium-fragment.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_Pal_crystal_S.webp",
+      },
+      {
+        name: "Ingot",
+        quantity: 50,
+        image: require("../assets/images/items/ingot.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_CopperIngot.webp",
+      },
+      {
+        name: "Cement",
+        quantity: 30,
+        image: require("../assets/images/items/cement.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_Cement.webp",
+      },
+      {
+        name: "Ancient Civilization Parts",
+        quantity: 10,
+        image: require("../assets/images/items/ancient-civilization-parts.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_PalCrystal_Ex.webp",
+      },
+    ],
+  },
+  {
+    ID: 4,
+    name: "Slider Module",
+    rarity: "Epic",
+    technology: 39,
+    description:
+      "Equipping it adds a slider spin to the sphere. \r\nThe sphere curves at a sharp angle, making it easier to catch Pals off guard and increasing capture power.Sphere Slider Capture Strength +3",
+    effects: ["Sphere Slider", "Capture Strength +3"],
+    image: require("../assets/images/items/slider-module.png"),
+    image_url:
+      "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_SphereModule_Curve2.webp",
+    recipe: [
+      {
+        name: "Paldium Fragment",
+        quantity: 100,
+        image: require("../assets/images/items/paldium-fragment.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_Pal_crystal_S.webp",
+      },
+      {
+        name: "Refined Ingot",
+        quantity: 50,
+        image: require("../assets/images/items/refined-ingot.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_IronIngot.webp",
+      },
+      {
+        name: "Carbon Fiber",
+        quantity: 50,
+        image: require("../assets/images/items/carbon-fiber.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_CarbonFiber.webp",
+      },
+      {
+        name: "Ancient Civilization Parts",
+        quantity: 15,
+        image: require("../assets/images/items/ancient-civilization-parts.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_PalCrystal_Ex.webp",
+      },
+    ],
+  },
+  {
+    ID: 5,
+    name: "Sniper Module Ⅱ",
+    rarity: "Legendary",
+    technology: 47,
+    description:
+      "Equipping it significantly increases the sphere's range, allowing it to sharply capture Pals and enhancing capture power.Sphere Range +2 Capture Strength +3",
+    effects: ["Sphere Range +2", "Capture Strength +3"],
+    image: require("../assets/images/items/sniper-module-Ⅱ.png"),
+    image_url:
+      "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_SphereModule_Sniper2.webp",
+    recipe: [
+      {
+        name: "Paldium Fragment",
+        quantity: 200,
+        image: require("../assets/images/items/paldium-fragment.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_Pal_crystal_S.webp",
+      },
+      {
+        name: "Pal Metal Ingot",
+        quantity: 50,
+        image: require("../assets/images/items/pal-metal-ingot.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_StealIngot.webp",
+      },
+      {
+        name: "Carbon Fiber",
+        quantity: 30,
+        image: require("../assets/images/items/carbon-fiber.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_CarbonFiber.webp",
+      },
+      {
+        name: "Ancient Civilization Parts",
+        quantity: 30,
+        image: require("../assets/images/items/ancient-civilization-parts.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_PalCrystal_Ex.webp",
+      },
+      {
+        name: "Ancient Civilization Core",
+        quantity: 5,
+        image: require("../assets/images/items/ancient-civilization-core.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_AncientParts2.webp",
+      },
+    ],
+  },
+  {
+    ID: 6,
+    name: "Homing Module",
+    rarity: "Legendary",
+    technology: 57,
+    description:
+      "Equipping it makes the sphere home in on Pals, further increasing capture power.Sphere Homing Capture Strength +3",
+    effects: ["Sphere Homing", "Capture Strength +3"],
+    image: require("../assets/images/items/homing-module.png"),
+    image_url:
+      "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_SphereModule_Homing.webp",
+    recipe: [
+      {
+        name: "Plasteel",
+        quantity: 50,
+        image: require("../assets/images/items/plasteel.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_Plastic.webp",
+      },
+      {
+        name: "Carbon Fiber",
+        quantity: 100,
+        image: require("../assets/images/items/carbon-fiber.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_CarbonFiber.webp",
+      },
+      {
+        name: "Ancient Civilization Parts",
+        quantity: 50,
+        image: require("../assets/images/items/ancient-civilization-parts.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_PalCrystal_Ex.webp",
+      },
+      {
+        name: "Dark Fragment",
+        quantity: 50,
+        image: require("../assets/images/items/dark-fragment.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_PalDarkParts.webp",
+      },
+      {
+        name: "Ancient Civilization Core",
+        quantity: 10,
+        image: require("../assets/images/items/ancient-civilization-core.png"),
+        image_url:
+          "https://cdn.paldb.cc/image/Others/InventoryItemIcon/Texture/T_itemicon_Material_AncientParts2.webp",
+      },
+    ],
+  },
+];
